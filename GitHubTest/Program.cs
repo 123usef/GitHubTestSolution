6e7492a0d6abe2a4cda83de21457852534c8073e
123usef <yousifm836@gmail>
2025-05-08 09:52:35 +0400

    Employee  Class Added

--- GitHubTest/Program.cs
+++ GitHubTest/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World to github ");
+            Console.WriteLine("Hello, World to github from joonguini!!");
         }
     }
 }
